e3a66e0f65f33490afe4a279d70790623267814f
Mirus Lu <miruslu@amazon.com>
2021-04-19 12:51:11 -0700

    Added license header

--- build_zips.sh
+++ build_zips.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
 
 rm -rf lib/lambda/lambda_layer/workspace
 mkdir -p lib/lambda/lambda_layer/workspace
